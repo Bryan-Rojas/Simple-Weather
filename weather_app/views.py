d5b8aae68115bc5b27a507771d5a2409096488e3
Bryan Rojas <bryroj14@gmail.com>
2018-12-25 23:54:21 -0800

    Updating views.py and front-end

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+import requests
+
 
 # Create your views here.
 
 def index(request):
-    url = ('')
+    url = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=98028dd10f4f33d1dda8948b10115977'
+    city = 'Hawthorne'
+    city_weather = requests.get(url.format(city)).json()
 
-    return render(request, 'weather_app/weather.html')
+    weather = {
+        'city' : city,
+        'temperature' : city_weather['main']['temp'],
+        'description' : city_weather['weather'][0]['description'],
+        'icon' : city_weather['weather'][0]['icon']
+    }
+
+    context = {'weather' : weather}
+    
+    return render(request, 'weather_app/weather.html', context)
