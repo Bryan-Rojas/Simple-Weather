f8eca0000c5920e831751f86e391dc5229640e31
Bryan Rojas <bryroj14@gmail.com>
2018-12-30 23:04:35 -0800

    Updated form

--- weather_app/views.py
+++ weather_app/views.py
@@ -9,11 +9,17 @@ def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=98028dd10f4f33d1dda8948b10115977'
     cities = City.objects.all()
 
+    if request.method == 'POST':
+        form = CityForm(request.POST) 
+        form.save() 
+
+    form = cityForm()
+
     weather_data = []
 
     for city in cities:
 
-        city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+        city_weather = requests.get(url.format(city)).json()
 
         weather = {
             'city' : city,
@@ -22,8 +28,8 @@ def index(request):
             'icon' : city_weather['weather'][0]['icon']
         }
 
-        weather_data.append(weather) #add the data for the current city into our list
+        weather_data.append(weather)
 
-    context = {'weather_data' : weather_data}
+    context = {'weather_data' : weather_data, 'form' : form}
 
     return render(request, 'weather_app/weather.html', context)
